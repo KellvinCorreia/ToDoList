c34fe4a802742349d230b4bd8a5dd7e6457daeae
Kellvin Correia Alves <kellvin.correia@gmail.com>
2022-10-26 20:51:57 -0300

    Add createTask

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
-let dataButton = document.querySelector('[data-form-button]')
-let dataInput = document.querySelector('[data-form-input]')
-dataButton.addEventListener('click', (e) => {
-  e.preventDefault();
+const dataButton = document.querySelector('[data-form-button]');
+const dataInput = document.querySelector('[data-form-input]');
+const dataList = document.querySelector('[data-list]');
+
+const createTask = event => {
+  event.preventDefault();
   if (dataInput.value) {
-    console.log(dataInput.value);
+    const task = document.createElement('li');
+    let content = `<p class="content">${dataInput.value}</p>`;
+    task.innerHTML = content;
+    task.classList.add('task');
+    dataList.appendChild(task);
+    dataInput.value = ''
+    dataInput.focus()
   }
-})
+};
+dataButton.addEventListener('click', createTask);
