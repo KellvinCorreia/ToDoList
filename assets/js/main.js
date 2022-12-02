bbbb4d9b198e0626b36ace06ff06fa4b5d581fd3
Kellvin Correia Alves <kellvin.correia@gmail.com>
2022-12-02 20:49:53 -0300

    Add formated date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,39 @@
-import DoneTaskButton from '../components/doneButton.js'
+import DoneTaskButton from '../components/doneButton.js';
 import DeleteTaskButton from '../components/deleteButton.js';
 
 const dataButton = document.querySelector('[data-form-button]');
 
-const createTask = event => {
-  const dataInput = document.querySelector('[data-form-input]');
-  const dataList = document.querySelector('[data-list]');
+const handleNewItem = event => {
   event.preventDefault();
-  if (dataInput.value) {
-    const task = document.createElement('li');
-    let content = `<p class="content">${dataInput.value}</p>`;
-    task.innerHTML = content;
-    task.classList.add('task');
-    dataList.appendChild(task);
-    task.appendChild(DoneTaskButton());
-    task.appendChild(DeleteTaskButton());
-    dataInput.value = '';
-    dataInput.focus();
+  const dataInput = document.querySelector('[data-form-input]');
+  const dataInputValue = dataInput.value;
+  const dataList = document.querySelector('[data-list]');
+  const calendar = document.querySelector('[data-form-date]');
+
+  moment.locale('pt-br');
+  const calendarDate = moment(calendar.value);
+  const formatedDate = calendarDate.format('LLL');
+
+  const settings = {
+    dataInputValue,
+    formatedDate
   }
+  
+  dataList.appendChild(createTask(settings));
+  dataInput.value = '';
+  dataInput.focus();
+
 };
-dataButton.addEventListener('click', createTask);
+
+const createTask = ({ dataInputValue, formatedDate }) => {
+  const task = document.createElement('li');
+  let content = `<p class="content">${formatedDate} | ${dataInputValue}</p>`;
+  task.innerHTML = content;
+  task.classList.add('task');
+  task.appendChild(DoneTaskButton());
+  task.appendChild(DeleteTaskButton());
+  return task;
+};
+
+
+dataButton.addEventListener('click', handleNewItem);
